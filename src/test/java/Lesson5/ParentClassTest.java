3ce7d6e2acaefdd55f88572681a7a0849fe65204
Alex <sandakovaalexx@mail.ru>
2022-09-16 23:44:19 +0800

    Lesson5

---
+++ src/test/java/Lesson5/ParentClassTest.java
@@ -0,0 +1,39 @@
+package Lesson5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class ParentClassTest {
+    WebDriverWait webDriverWait;
+    WebDriver driver;
+    Actions actions;
+
+    @BeforeAll
+    static void registerDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupBrowser() {
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        }
+}
